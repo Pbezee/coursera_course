37b17bb70a2256814cca92e93f22e0fbe5fa29db
Pyae Bhone Zaw <pyaebhonezaw2003@gmail.com>
2023-10-28 23:46:56 +0630

    new

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pyae Bhone Zaw PHP</title>
+</head>
+
+<body>
+    <h1>Pyae Bhone Zaw Reauest/ Response</h1>
+    <label>The SHA256 hash of "Pyae Bhone Zaw" is</label>
+    <?php
+    print hash('sha256', 'Pyae Bhone Zaw');
+    ?>
+    <br>
+    <pre>
+        **************
+        **          **
+        **          **
+        **************
+        **
+        **
+        **
+    </pre>
+
+    <a href="fail.php">Click here to check the error setting</a>
+    <br>
+    <a href="check.php">Click here to cause a traceback</a>
+
+
+</body>
+
+</html>
